ac084eb69ed3478b484d4979019a718b9eb3a5e1
Admir Husovic <mail.admir.husovic@gmail.com>
2015-09-16 12:42:10 +0200

    work in changes

--- js/app/classes/Game.js
+++ js/app/classes/Game.js
@@ -1,77 +1,78 @@
-define(['Display', 'Sprite', 'Events', 'Player'], function( Display, Sprite, Events, Player) {
+define(['Handler', 'Sprite', 'Events', 'Player'], function(Handler, Sprite, Events, Player) {
 
-    var _display = new Display({width: 640, height: 640}),
-        _running = false,
-        _fps = 1 / 50;
+  var _running = false,
+    _fps = 1 / 50;
 
-    var definePlayer = {
-        pos: {x: 320, y: 400},
-        speed: 150,
-        width: 30,
-        height: 40,
-        sprite: new Sprite({
-          name: 'Hero',
-          url: 'assets/dude_animation_sheet.png',
-          width:130,
-          height:130,
-          state:{
-            // TODO function för att pendla mellan crop värden
-            idle:[40,40],
-            walk:[60,60],
-            run:[70,70]
-          },
-        })
-    };
+  var definePlayer = {
+    pos: {
+      x: 320,
+      y: 400
+    },
+    speed: 150,
+    width: 42,
+    height: 54,
+    sprite: new Sprite({
+      name: 'Hero',
+      url: 'assets/sprites.png',
+      width: 42,
+      height: 54,
+      state: {
+        idle: [40, 40],
+        walk: [60, 60],
+        run: [70, 70]
+      }
+    })
+  };
 
-    function Game() {
-        this.graphics = _display.getGfx();
-        this.width = _display.getWidth();
-        this.height = _display.getHeight();
-        this.player = new Player(definePlayer);
+  function Game() {
+    this.graphics = Handler.getGfx();
+    this.width = Handler.getDisplayWidth();
+    this.height = Handler.getDisplayHeight();
+    this.player = new Player(definePlayer);
+  }
+
+  Game.prototype = {
+    init: function() {
+      if (_running) return;
+      //Else start the game
+      _running = true;
+      this.main();
+    },
+
+    main: function() {
+      var self = this;
+
+      function loop() {
+        self.update(_fps);
+        self.render(self.graphics);
+        window.requestAnimationFrame(loop);
+      }
+      loop();
+    },
+
+    update: function(delta) {
+      this.player.update(delta);
+    },
+
+    render: function(gfx) {
+      //Clear the screen of previous renders
+      gfx.clearRect(0, 0, this.width, this.height);
+      gfx.fillStyle = "#2b303b";
+      gfx.fillRect(0, 0, this.width, this.height);
+      this.player.render(gfx);
+    },
+    //Getter
+    isRunning: function() {
+      return _running;
+    },
+
+    //Setter
+    toggleRunning: function() {
+      _running = !_running; //Flip state between true/false
     }
 
-    Game.prototype = {
-        init: function() {
-            if(_running) return;
-            //Else start the game
-            _running = true;
-            console.log(this.player);
-            this.main();
-        },
+  };
 
-        main: function() {
-            var self = this;
-            function loop() {
-                self.update( _fps );
-                self.render( self.graphics );
-                window.requestAnimationFrame( loop );
-            }
-            loop();
-        },
-
-        update: function( delta ) {
-            this.player.update( delta );
-        },
-
-        render: function( gfx ) {
-            //Clear the screen of previous renders
-            gfx.clearRect(0,0,this.width, this.height);
-            gfx.fillStyle = "#2b303b";
-            gfx.fillRect(0,0,this.width, this.height);
-            this.player.render(gfx);
-        },
-        //Getter
-        isRunning: function() {
-            return _running;
-        },
-
-        //Setter
-        toggleRunning: function() {
-            _running = !_running; //Flip state between true/false
-        }
-
-    };
-
-    return Game;
+  return Game;
 
 });
--- js/app/entities/Player.js
+++ js/app/entities/Player.js
@@ -1,11 +1,11 @@
-define(['Entity', 'Bullet', 'Input', 'Utils'], function(Entity, Bullet, Input, Utils) {
+define(['Entity', 'Bullet', 'Input', 'Sprite'], function(Entity, Bullet, Input, Sprite) {
 
     var _input = new Input(); // Put in a global handler
 
     var velocity = {x: 0, y: 0};
-    var acceleration = 10;
-    var friction = 3;
-    var maxSpeed = 160;
+    var acceleration = 30;
+    var friction = 10;
+    var maxSpeed = 200;
 
     var bullets = [];
     //Bullet logic, move out of this class
@@ -17,10 +17,18 @@ define(['Entity', 'Bullet', 'Input', 'Utils'], function(Entity, Bullet, Input, U
 
     var bulletOptions = {
         pos: {x: 0, y: 0},
-        speed: 250,
-        width: 10,
-        height: 10,
-        sprite: null
+        speed: 300,
+        width: 8,
+        height: 22,
+        sprite: new Sprite({
+            name: 'Bullet',
+            url: 'assets/sprites.png',
+            width: 8,
+            height: 22,
+            state: {
+                idle:[40,40]
+            }
+        })
     };
 
     function Player(obj) {
@@ -37,7 +45,7 @@ define(['Entity', 'Bullet', 'Input', 'Utils'], function(Entity, Bullet, Input, U
     };
 
     Player.prototype.update = function(delta) {
-
+        var self = this;
         //Keyboard input
         calculateVelocity();
         this.pos.y += velocity.y * delta;
@@ -55,6 +63,9 @@ define(['Entity', 'Bullet', 'Input', 'Utils'], function(Entity, Bullet, Input, U
             if(bullet.pos.y === 0) {
                 arr.splice(i, 1);
             }
+            if(self.collides(bullet)) {
+                console.log('friendly fire');
+            }
         });
 
         //Check bounds
@@ -64,7 +75,24 @@ define(['Entity', 'Bullet', 'Input', 'Utils'], function(Entity, Bullet, Input, U
     Player.prototype.render = function( gfx ) {
         //Create rectangle
 
-        gfx.drawImage(this.sprite.div,0,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height)
+
+        console.log(velocity);
+        if(velocity.x > 0 && velocity.x < 100) {
+            gfx.drawImage(this.sprite.div,142,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height);
+        }
+        else if(velocity.x >=100) {
+            gfx.drawImage(this.sprite.div,192,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height);
+        }
+        else if(velocity.x < 0 && velocity.x > -100) {
+            gfx.drawImage(this.sprite.div,46,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height);
+        }
+        else if(velocity.x <= -100) {
+            gfx.drawImage(this.sprite.div,0,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height);
+        }
+        else {
+            gfx.drawImage(this.sprite.div,94,0,this.sprite.width,this.sprite.height,this.pos.x,this.pos.y,this.width,this.height);
+        }
+
         bullets.forEach(function(bullet) {
             bullet.render( gfx );
         });
